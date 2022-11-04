b377b66741eb5c413b79296fc7b302d829515e7e
Dallas Vaughn <dallasvaughn@Dallass-MBP.hsd1.wa.comcast.net>
2022-11-04 15:22:26 -0700

    added Head to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import BookPreview from '../components/BookPreview';
 import Loader from '../components/Loader';
+import Head from 'next/head';
 
 export interface Book {
   id: string;
@@ -35,47 +36,51 @@ const Home: NextPage = () => {
     setTriedSearch(true);
     setLoading(false);
   };
-  console.log(results);
 
   return (
-    <div className="flex min-h-screen max-w-screen-lg flex-col items-center mx-auto">
-      <h1 className="text-5xl font-bold text-emerald-600 mt-20 mb-10">
-        Google Books
-      </h1>
-      <div className="flex w-[90%]">
-        <input
-          type="text"
-          className="border border-r-0 py-2 px-6 rounded-l-full w-[768px] outline-none"
-          placeholder="Search for books... (e.g. harry potter)"
-          value={search}
-          onChange={(e) => {
-            const input = e.target as HTMLInputElement;
-            setSearch(input.value);
-          }}
-        />
-        <button
-          className="bg-emerald-600 text-white py-2 px-6 rounded-r-full hover:opacity-75"
-          onClick={handleClick}
-        >
-          Search
-        </button>
+    <>
+      <Head>
+        <title>Books</title>
+      </Head>
+      <div className="flex min-h-screen max-w-screen-lg flex-col items-center mx-auto">
+        <h1 className="text-5xl font-bold text-emerald-600 mt-20 mb-10">
+          Google Books
+        </h1>
+        <div className="flex w-[90%]">
+          <input
+            type="text"
+            className="border border-r-0 py-2 px-6 rounded-l-full w-[768px] outline-none"
+            placeholder="Search for books... (e.g. harry potter)"
+            value={search}
+            onChange={(e) => {
+              const input = e.target as HTMLInputElement;
+              setSearch(input.value);
+            }}
+          />
+          <button
+            className="bg-emerald-600 text-white py-2 px-6 rounded-r-full hover:opacity-75"
+            onClick={handleClick}
+          >
+            Search
+          </button>
+        </div>
+        {loading && <Loader />}
+        {triedSearch && results.length === 0 ? (
+          <p className="mt-10">
+            No search results found. Try searching for another book.
+          </p>
+        ) : (
+          results.length > 0 && (
+            <div className="flex flex-wrap gap-10 mt-10 p-6">
+              {results.length > 0 &&
+                results.map((result, i) => {
+                  return <BookPreview key={i} book={result} />;
+                })}
+            </div>
+          )
+        )}
       </div>
-      {loading && <Loader />}
-      {triedSearch && results.length === 0 ? (
-        <p className="mt-10">
-          No search results found. Try searching for another book.
-        </p>
-      ) : (
-        results.length > 0 && (
-          <div className="flex flex-wrap gap-10 mt-10 p-6">
-            {results.length > 0 &&
-              results.map((result, i) => {
-                return <BookPreview key={i} book={result} />;
-              })}
-          </div>
-        )
-      )}
-    </div>
+    </>
   );
 };
 
